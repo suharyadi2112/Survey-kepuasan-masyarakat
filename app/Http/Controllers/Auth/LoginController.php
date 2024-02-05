bc59c70d1c567d5f0da69a9d043ad6d606cafb6c
suharyadi <yadisuhar10@gmail.com>
2024-02-05 14:30:08 +0700

    - fixing

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,8 +26,9 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::INDEXLAPORAN;
+    
     /**
      * Create a new controller instance.
      *
